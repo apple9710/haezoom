780fc7e7715a896056a8c200c9b29609228eb2d5
hs <hyhshy12@naver.com>
2025-07-24 16:31:32 +0900

    인증 과정 제거

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import { useAuthStore } from '@/stores/auth'
 import './assets/main.css'
 
 const app = createApp(App)
@@ -11,9 +10,4 @@ const pinia = createPinia()
 app.use(pinia)
 app.use(router)
 
-// 앱 마운트 후 인증 정보 초기화
 app.mount('#app')
-
-// 인증 정보 복원
-const authStore = useAuthStore()
-authStore.initAuth()
